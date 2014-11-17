0ac9ed7bc8de0e203e9363060b1d2aae88f7bd22
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-11-17 13:30:41 -0500

    Copy and watch html/css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var browserify = require('browserify');
 var transform  = require('vinyl-transform');
 
 var paths = {
-  scripts: "app/scripts/*.js"
+  scripts: "app/scripts/*.js",
+  html:    "app/*.html",
+  styles:  "app/styles/*.css"
 }
 
 gulp.task("browserify", function() {
@@ -16,8 +18,21 @@ gulp.task("browserify", function() {
              .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task("watch", function() {
-  gulp.watch(paths.scripts, ["browserify"]);
+gulp.task("html", function() {
+  return gulp.src([paths.html])
+              .pipe(gulp.dest("./public"))
 })
 
-gulp.task("default", ["browserify", "watch"]);
+gulp.task("styles", function() {
+  return gulp.src([paths.styles])
+              .pipe(gulp.dest("./public/css"))
+})
+
+gulp.task("watch", function() {
+  gulp.watch(paths.scripts, ["browserify"]);
+  gulp.watch(paths.html, ["html"]);
+  gulp.watch(paths.styles, ["styles"]);
+})
+
+gulp.task("copy", ["html", "styles"]);
+gulp.task("default", ["browserify", "copy", "watch"]);
