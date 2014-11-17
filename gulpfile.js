ae3a296bc2b4fdd66dedd10c48be1016cfa8168c
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-11-17 13:22:23 -0500

    Big Browserify is watching you

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@ var gulp       = require('gulp');
 var browserify = require('browserify');
 var transform  = require('vinyl-transform');
 
+var paths = {
+  scripts: "app/scripts/*.js"
+}
+
 gulp.task("browserify", function() {
   var browserified = transform(function(filename) {
     return browserify(filename).bundle();
   });
 
-  return gulp.src(["./app/scripts/*.js"])
+  return gulp.src([paths.scripts])
              .pipe(browserified)
              .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task("watch", function() {
+  gulp.watch(paths.scripts, ["browserify"]);
 })
+
+gulp.task("default", ["browserify", "watch"]);
