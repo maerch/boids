4ace1c346fc2ff32d8a32e8384ea557822fb715c
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-11-23 17:04:53 -0500

    Adds max level

--- app/scripts/quadtree.js
+++ app/scripts/quadtree.js
@@ -1,4 +1,4 @@
-function Quadtree(x1, y1, x2, y2) {
+function Quadtree(x1, y1, x2, y2, level) {
   this.x1 = x1;
   this.x2 = x2;
   this.y1 = y1;
@@ -8,17 +8,18 @@ function Quadtree(x1, y1, x2, y2) {
   this.nodes   = [];
   this.leaf    = true;
 
+  this.level   = level || 1;
 }
 
 Quadtree.prototype = {
-  MAX_OBJECTS: 5
+  MAX_OBJECTS: 5,
+  MAX_LEVEL:   5
 }
 
 Quadtree.prototype.insert = function(x, y) {
   if(isNaN(x) || isNaN(y)) return;
-
   if(this.leaf) {
-    if(this.objects.length<this.MAX_OBJECTS) {
+    if(this.objects.length<this.MAX_OBJECTS || this.level === this.MAX_LEVEL) {
       this.objects.push({x: x, y: y});
       return this;
     } else {
@@ -28,10 +29,10 @@ Quadtree.prototype.insert = function(x, y) {
   } else {
     var upper = (y<(y2-y1)/2);
     var left  = (x<(x2-x1)/2);
-    if(upper && left)  this.nodes[0].insert(x, y) else 
-    if(upper)          this.nodes[1].insert(x, y) else
-    if(lower && left)  this.nodes[2].insert(x, y) else
-    if(lower)          this.nodes[3].insert(x, y)
+    if(upper && left)  return this.nodes[0].insert(x, y);
+    if(upper)          return this.nodes[1].insert(x, y);
+    if(lower && left)  return this.nodes[2].insert(x, y);
+    if(lower)          return this.nodes[3].insert(x, y);
   }
 }
 
